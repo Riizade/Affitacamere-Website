6dc60caa85ee72b11226d2662a8da36937994e2f
Adam James-Liebau Johnson <riizade@gmail.com>
2016-03-01 23:11:13 -0500

    changed strings.html to strings.js containing an object

--- assets/js/language.js
+++ assets/js/language.js
@@ -6,14 +6,12 @@ function getCurrentLanguage() {
 $('document').ready(function(){ // on page load
   var language = getCurrentLanguage(); // get the current language from the URL
   $("#hidden").load("../assets/strings.html", function() { // load strings.html into the hidden attribute
-    $(".string." + language).each(function(index) { // for each string class element with the current language as one of its other classes
+    $(".has-string").each(function(index) { // for each string class element with the current language as one of its other classes
       var classes = $(this).attr('class').split(/\s+/); // get the element's classes
       stringElement = this; // keep track of the element object when "this" changes context
       for (i = 0; i < classes.length; i++) { // for each of the element's classes
-        if (classes[i] != language && classes[i] != "string") { // if the class is not the one used to select it, or string (meaning: the string-[identifier] class)
-          $("." + classes[i] + ":not(.string)").each(function(index) { // for each non-string element sharing the class
-            $(this).text($(stringElement).text()); // replace the text with the text from the string element
-          })
+        if (/^string-/.test(classes[i])) { // if the class begins with string-
+          $(this).text(strings[classes[i]][language]); // replace the text with the corresponding string from the strings object
         }
       }
     })
---
+++ assets/strings.js
@@ -0,0 +1,40 @@
+var strings = {
+  "string-navbar-title": {
+    "en": "Villa Agata",
+    "it": "Villa Agata"
+  },
+
+  "string-homepage": {
+    "en": "Home",
+    "it": "Pagina Iniziale"
+  },
+
+  "string-aboutpage": {
+    "en": "About",
+    "it": "Traduzione"
+  },
+
+  "string-contactpage": {
+    "en": "Contact",
+    "it": "Contatto"
+  },
+
+  "string-language": {
+    "en": "Language",
+    "it": "Lingua"
+  },
+
+  "string-greeting": {
+    "en": "Welcome!",
+    "it": "Benvenuto!"
+  },
+
+  "string-greeting-message": {
+    "en": "This is the finest bed and breakfast in all of Levanto. You won't find anything better. Seriously. I promise. This message is long for testing purposes. I could go on forever. For testing purposes. Because again, this is a test. Of the site. And stuff.",
+    "it": "Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano Italiano."
+  }
+};
+
+$('document').ready(function(){ // on page load
+  console.log(strings);
+});
